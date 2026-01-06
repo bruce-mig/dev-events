a116668f34892f5678766bedd7abc332da7c0c8b
bruce-mig <brucetmig@icloud.com>
2026-01-06 14:00:04 +0200

    fix: update event type to IEvent and fetch events from API

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
-import { events, EventItem } from "@/lib/constants"
+import { IEvent } from "@/database";
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`)
+
+  const { events } = await response.json()
+
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&apos;t Miss</h1>
@@ -14,7 +20,7 @@ const Page = () => {
         <h3>Featured Events</h3>
 
         <ul className="events">
-          {events && events.length > 0 && events.map((event: EventItem) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title} className="list-none">
               <EventCard {...event} />
             </li>
