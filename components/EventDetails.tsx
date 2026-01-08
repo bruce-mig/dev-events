fa0baf45d72c370d9a2cc45fa49e4c2fbfb04984
bruce-mig <brucetmig@icloud.com>
2026-01-08 09:56:11 +0200

    refactor: improve layout structure and semantics in EventDetails component

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -12,7 +12,7 @@ import { cacheLife } from "next/cache";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 const EventDetailItem = ({ icon, alt, label }: { icon: string, alt: string, label: string }) => (
-   <div className="flex flex-row gap-2 items-center">
+   <div className="flex-row-gap-2 items-center">
       <Image src={icon} alt={alt} width={17} height={17} />
       <p>{label}</p>
    </div>
@@ -82,55 +82,56 @@ const EventDetails = async ({ params }: { params: Promise<string> }) => {
          <div className="header">
             <h1>Event Description</h1>
             <p>{description}</p>
-
-            <div className="details">
-               {/* Left Side - Event Content */}
-               <div className="content">
-                  <Image src={image} alt="Event Banner" width={800} height={800} className="banner" />
-
-                  <section className="flex-col-gap-2">
-                     <h2>Overview</h2>
-                     <p>{overview}</p>
-                  </section>
-
-                  <section className="flex-col-gap-2">
-                     <h2>Event Details</h2>
-                     <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
-                     <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
-                     <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
-                     <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
-                     <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
-                  </section>
-
-                  <EventAgenda agendaItems={agenda} />
-
-                  <section className="flex-col-gap-2">
-                     <h2>About the Organizer</h2>
-                     <p>{organizer}</p>
-                  </section>
-
-                  <EventTags tags={tags} />
-
-
-               </div>
-
-               {/* Right Side - Booking Form */}
-               <aside className="booking">
-                  <div className="signup-card">
-                     <h2>Book Your Spot</h2>
-                     {bookings > 0 ? (
-                        <p className="text-sm">
-                           Join {bookings} people who have already booked their spot!
-                        </p>
-                     ) : (
-                        <p className="text-sm">Be the first to book your spot!</p>
-                     )}
-
-                     <BookEvent eventId={event._id} slug={event.slug} />
-                  </div>
-               </aside>
-            </div>
          </div>
+
+         <div className="details">
+            {/* Left Side - Event Content */}
+            <div className="content">
+               <Image src={image} alt="Event Banner" width={800} height={800} className="banner" />
+
+               <section className="flex-col-gap-2">
+                  <h2>Overview</h2>
+                  <p>{overview}</p>
+               </section>
+
+               <section className="flex-col-gap-2">
+                  <h2>Event Details</h2>
+                  <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
+                  <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
+                  <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
+                  <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
+                  <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
+               </section>
+
+               <EventAgenda agendaItems={agenda} />
+
+               <section className="flex-col-gap-2">
+                  <h2>About the Organizer</h2>
+                  <p>{organizer}</p>
+               </section>
+
+               <EventTags tags={tags} />
+
+
+            </div>
+
+            {/* Right Side - Booking Form */}
+            <aside className="booking">
+               <div className="signup-card">
+                  <h2>Book Your Spot</h2>
+                  {bookings > 0 ? (
+                     <p className="text-sm">
+                        Join {bookings} people who have already booked their spot!
+                     </p>
+                  ) : (
+                     <p className="text-sm">Be the first to book your spot!</p>
+                  )}
+
+                  <BookEvent eventId={event._id} slug={event.slug} />
+               </div>
+            </aside>
+         </div>
+
 
          <div className="flex w-full flex-col gap-4 pt-20">
             <h2>Similar Events</h2>
